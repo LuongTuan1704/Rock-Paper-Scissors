c77a2c7e6ec947297a25c553891b848d2ed07978
LuongTuan <62272226+LuongTuan1704@users.noreply.github.com>
2020-09-30 17:38:58 +0700

    fix ver1 OK

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,3 +1,90 @@
+
+const boardV1 = document.querySelector('.game_body--board-v1 .options');
+const items =Array.from(boardV1.children) 
+const player = document.querySelector('.player_picked_item');
+const computer = document.querySelector('.compute_picked_item');
+const itemNames = ['paper', 'rock', 'scissors'];
+let score = 0,computeScore = 0;
+const board = document.querySelector('.game_body--board-v1');
+const boardCheck = document.querySelector('.game_chossed');
+items.forEach(element => {
+    element.onclick= function(){      
+        board.classList.add("hide");
+        boardCheck.classList.remove("hide");
+        let playerPicked = element.getAttribute('value');
+            showItem(playerPicked, 'player');
+           let cumputePicked = computePicked();
+           checkWiner(playerPicked, cumputePicked)
+        
+    }
+});
+
+function showItem(item, type) {
+    if (type === 'player') {
+        player.classList.add('slide-left')
+        player.innerHTML = `<div class="box ${item}"> <img src="./../src/images/icon-${item}.svg" alt="fail load ${item}.svg file"></div>`;
+        setTimeout(function () {
+            player.classList.remove('slide-left')
+        }, 500)
+    } else {
+        computer.classList.add('slide-right')
+        computer.innerHTML = `<div class="box ${item}"> <img src="./../src/images/icon-${item}.svg" alt="fail load ${item}.svg file"></div>`;
+        setTimeout(function(){
+            computer.classList.remove('slide-right')
+        },500)
+        
+    }
+}
+
+
+function computePicked() {
+    let randomItem = Math.floor(Math.random() * itemNames.length);
+    showItem(itemNames[randomItem]);
+    return itemNames[randomItem];
+}
+const checks = {
+    'paper': 'rock',
+    'rock': 'scissors',
+    'scissors': 'paper'
+}
+const status = document.querySelector('.status');
+function checkWiner(play, compute) {
+    let str = '';
+    if (play === compute) {
+        str = 'Draw';
+        setTimeout(function () {
+            player.classList.add('rotate')
+            computer.classList.add('rotate');
+            document.querySelector('.btn_status').classList.remove('hide');
+        }, 500)
+    } else if (checks[play] === compute) {
+        str = 'You Win';
+        document.querySelector('#player #score').innerText = ++score;
+        setTimeout(function(){player.children.item(0).classList.add('animate');
+        document.querySelector('.btn_status').classList.remove('hide');
+    },500)
+        
+    } else {
+        str = "You Lose"
+        document.querySelector('#compute  #score').innerText = ++computeScore; 
+        setTimeout(function () { computer.children.item(0).classList.add('animate');
+        document.querySelector('.btn_status').classList.remove('hide');
+     }, 500)
+    }
+   
+    status.innerHTML = str.includes('Win') ? `<span class ='wincl'>${str}</span>` : str.includes('Lose') ? `<span  class ='losecl'>${str}</span>` : `<span  class ='drawcl'>${str}</span>`;
+
+}
+
+ document.querySelector('.btn_play_again').onclick = function () {
+     board.classList.remove("hide")
+     boardCheck.classList.add("hide")
+     player.classList = 'player_picked_item';
+     computer.classList ='compute_picked_item';
+     document.querySelector('.btn_status').classList.add('hide')
+}
+
+
 const btnGameRules = document.getElementById('game_rules');
 const WiewRules = document.querySelector('.game-rules-wiew');
 btnGameRules.onclick = function () {
@@ -5,77 +92,4 @@ btnGameRules.onclick = function () {
 }
 WiewRules.onclick = function () {
     WiewRules.classList.remove('open')
-}
-document.querySelector('.btn_play_again').onclick = function () {
-    document.querySelector('.pos_center').classList.remove("hide")
-    document.querySelector('.game_chossed').classList.add("hide")
-    pl.classList.remove('animate')
-    cp.classList.remove('animate')
-}
-const item = document.querySelectorAll('.s');
-item.forEach(item => {
-    item.onclick = function () {
-        document.querySelector('.pos_center').classList.add("hide")
-        document.querySelector('.game_chossed').classList.remove("hide")
-        let playerPicked = item.getAttribute('value');
-        showPickItem(playerPicked, 'player');
-        let cumputePicked = computePicked();
-        checkWinter(playerPicked, cumputePicked)
-    }
-})
-
-const pl = document.querySelector('.player_picked_item');
-const cp = document.querySelector('.compute_picked_item');
-const Color = {
-    'paper': ['#506cf4', 'linear-gradient(#4865f4, #5671f5)', '0px 8px 0px -1px #2642bf'],
-    'rock': ['#dc3853', 'linear-gradient(#dc2e4e, #dd405d)', '0px 8px 0px -1px #991635'],
-    'scissors': ['#eaa81e', 'linear-gradient(#ec9e0e, #eca922)', '0px 8px 0px -1px #cc6d1b']
-};
-
-function showPickItem(item, type) {
-    if (type === 'player') {
-        pl.innerHTML = `<img src="./images/icon-${item}.svg" alt="" srcset="">`
-        pl.style.setProperty("border-color", Color[item][0])
-        pl.style.setProperty('background-image', Color[item][1])
-        pl.style.setProperty('box-shadow', Color[item][2])
-
-    } else {
-        cp.innerHTML = `<img src="./images/icon-${item}.svg" alt="" srcset="">`
-        cp.style.setProperty("border-color", Color[item][0])
-        cp.style.setProperty('background-image', Color[item][1])
-        cp.style.setProperty('box-shadow', Color[item][2])
-    }
-}
-const itemNames = ['paper', 'rock', 'scissors'];
-let score = 0,
-    computeScore = 0;
-
-function computePicked() {
-    let randomItem = Math.floor(Math.random() * itemNames.length)
-    showPickItem(itemNames[randomItem], '')
-    return itemNames[randomItem];
-}
-//paper > rock > scissors 
-const checks = {
-    'paper': 'rock',
-    'rock': 'scissors',
-    'scissors': 'paper'
-}
-
-function checkWinter(play, compute) {
-    let status = '';
-    if (play === compute) {
-        status = 'Draw';
-    } else if (checks[play] === compute) {
-        status = 'You Win';
-        document.getElementById('score').innerText = ++score
-        pl.classList.add('animate')
-    } else {
-        status = "You Lose"
-        document.getElementById('computer_score').innerText = ++computeScore;
-        cp.classList.add('animate')
-    }
-    document.querySelector('.btn_status').classList.remove('hide')
-    document.querySelector('.status').innerText = status
-
 }
